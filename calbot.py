2b6abb9c7fb3764fd2d741f708a4a3f3a6ad65f9
yenissi <154251432+yenissi@users.noreply.github.com>
2023-12-20 14:36:30 +0800

    Update calbot.py

--- calbot.py
+++ calbot.py
@@ -2,6 +2,7 @@ import json
 import random
 from difflib import get_close_matches
 
+#pullreqnadis
 #nagtry lang ako maggithub AHAHAH
 #nagtatry kung gagana 
 def load_knowledge_base(file_path: str) -> dict:
